e1598bcf14a130b0342bda360dda97bc4bd289ee
Максим Матросов <matrosov.02@bk.ru>
2021-09-17 01:12:17 +0400

    конструктор

--- АСД1/sroke.cpp
+++ АСД1/sroke.cpp
@@ -5,8 +5,12 @@ private:
 	char* _str;
 	unsigned _size;
 public:
-	string(const char* s) {};
-	~string() {};
+	string(const char* s){
+		_size = strlen(s);
+		_str = new char[_size + 1];
+		strcpy(_str, s);
+	};
+	~string() { delete[]_str; };
 	unsigned Get_size()
 	{
 		return _size;
@@ -16,5 +20,8 @@ public:
 		if ((index > _size - 1) || (index < 0) || (_size == 0))throw "Incorrect index!";
 		return _str[index];
 	}
-	
+	string operator +(const string& rhs)
+	{
+
+	}
 };
